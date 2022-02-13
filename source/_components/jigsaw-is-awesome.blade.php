6bc6dce0823ad017ca643c04d62b24bb002678c2
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-02-12 19:59:38 -0500

    Use `ln` instead of `line-number` for our own line numbers

--- source/_components/jigsaw-is-awesome.blade.php
+++ source/_components/jigsaw-is-awesome.blade.php
@@ -2,53 +2,53 @@
     <div class="flex flex-col w-full md:w-4/5 lg:w-2/3 -mr-2">
         @component('_components.code-editor')
             <div class="editor-row">
-                <p class="line-number">1</p>
+                <p class="ln">1</p>
                 <div class="line-code">---</div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">2</p>
+                <p class="ln">2</p>
                 <div class="line-code">
                     <span class="text-pink">title</span>: <span class="text-yellow">"Jigsaw is awesome!"</span>
                 </div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">3</p>
+                <p class="ln">3</p>
                 <div class="line-code">---</div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">5</p>
+                <p class="ln">5</p>
                 <div class="line-code">@@extends('layouts.master')</div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">6</p>
+                <p class="ln">6</p>
                 <div class="line-code"></div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">7</p>
+                <p class="ln">7</p>
                 <div class="line-code">@@section('content')</div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">8</p>
+                <p class="ln">8</p>
                 <div class="line-code ml-4">
                     <span class="text-pink">&lt;h1&gt;</span>@{{ $page->title }}<span class="text-pink">&lt;/h1&gt;</span>
                 </div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">9</p>
+                <p class="ln">9</p>
                 <div class="line-code ml-4">
                     <span class="text-pink">&lt;p&gt;</span>Contact us at <br class="hidden md:block lg:hidden"> @{{ $page->contact_email }}<span class="text-pink">&lt;/p&gt;</span>
                 </div>
             </div>
 
             <div class="editor-row">
-                <p class="line-number">10</p>
+                <p class="ln">10</p>
                 <div class="line-code">@@endsection</div>
             </div>
         @endcomponent
