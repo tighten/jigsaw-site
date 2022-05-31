a00a97edeaadb73c4aecdd14c7510f12c2318e61
Sara Bine <sara@sarabine.com>
2022-05-31 16:18:26 -0600

    Add FieldGoal section to the homepage

---
+++ source/_components/fieldgoal.blade.php
@@ -0,0 +1,17 @@
+<section class="bg-brown-lightest py-12 md:py-16 px-4 sm:px-6">
+    <div class="container-content flex flex-col items-center text-center">
+        <div class="w-full max-w-xs flex justify-between items-center mb-12">
+            <img class="w-1/3 p-1 bg-white rounded-lg shadow border-2 border-white" src="/assets/img/jigsaw-logo.svg" alt="Jigsaw logo">
+            <span class="font-bold text-blue-darker text-4xl">+</span>
+            <img class="w-1/3" src="/assets/img/fieldgoal-logo.svg" alt="FieldGoal logo">
+        </div>
+
+        <h2 class="text-blue-darker text-2xl md:text-4xl mb-6">Don't build a whole backend<br>just for one stupid form</h2>
+
+        <p class="text-grey-dark text-lg mb-10">Is one stupid form preventing you from<br>building a static site with Jigsaw?</p>
+
+        <p class="font-bold text-lg mb-10">FieldGoal solves that.</p>
+
+        <a href="https://fieldgoal.io" class="hover:bg-grey-light text-grey-darker hover:text-purple-dark no-underline tracking-wide uppercase rounded-lg text-sm bg-grey-lightest shadow-md hover:shadow-lg py-4 px-6">Visit fieldgoal.io</a>
+    </div>
+</section>
