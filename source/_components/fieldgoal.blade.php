7419f37ba1b8e690bbab1beb7665f2fcaf9e8a4a
Sara Bine <sara@sarabine.com>
2022-05-31 19:32:34 -0600

    Update to match newer mockup

--- source/_components/fieldgoal.blade.php
+++ source/_components/fieldgoal.blade.php
@@ -6,11 +6,11 @@
             <img class="w-1/3" src="/assets/img/fieldgoal-logo.svg" alt="FieldGoal logo">
         </div>
 
-        <h2 class="text-blue-darker text-2xl md:text-4xl font-normal mb-6">Don't build a whole backend<br>just for one stupid form.</h2>
+        <h2 class="text-blue-darker text-2xl md:text-4xl font-normal mb-6">Don't build a whole backend<br> just for one stupid form.</h2>
 
-        <p class="text-grey-dark text-lg mb-10">Is one stupid form preventing you from<br>building a static site with <span class="font-bold">Jigsaw?</span></p>
+        <p class="text-grey-dark text-lg mb-10">Is that one form on your site holding you back<br class="hidden sm:block"> from building a static site with Jigsaw?</p>
 
-        <p class="font-bold text-lg mb-10">FieldGoal solves that.</p>
+        <p class="font-bold text-blue-darker text-lg mb-10">FieldGoal solves that exact problem.</p>
 
         <a href="https://fieldgoal.io"  title="Visit fieldgoal.io"
             class="hover:bg-grey-light text-grey-darker hover:text-purple-dark no-underline tracking-wide uppercase rounded-lg text-sm bg-grey-lightest shadow-md hover:shadow-lg py-4 px-6">
